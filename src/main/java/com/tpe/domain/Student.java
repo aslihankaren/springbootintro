0831c9a8f5259ce581ed33a8f8bd1b5a2da3fccd
aslihankaren <fatmacinaryalcin@hotmail.com>
2024-05-10 19:22:16 +0200

    devam

--- src/main/java/com/tpe/domain/Student.java
+++ src/main/java/com/tpe/domain/Student.java
@@ -9,46 +9,45 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
-
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
 @Entity
-public class Student { //student tablsou olustu
+public class Student { // student
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Setter(AccessLevel.NONE)
-private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
 
-@NotBlank(message = "first name can not be white space")
-@NotNull(message = "first name can not be null")
-@Size(min= 2, max = 25,message = "first name '${validatedValue}' must be between {min} and  {max}chars")
-@Column(nullable = false, length = 25)
-private  String name;
+    @NotNull(message = "first name can not be null")
+    @NotBlank(message = "first name can not be white space")
+    @Size(min = 2, max = 25, message = "First name '${validatedValue}' must be between {min} and {max} chars")
+    @Column(nullable = false, length = 25) //
+    private String name;
 
+    @Column(nullable = false, length = 25)
+    private String lastName;
 
-@Column(nullable = false, length = 25)
-private  String lastname;
+    @Column
+    private Integer grade;
 
+    @Column(nullable = false, length = 50, unique = true)
+    @Email(message = "Provide valid email")
+    private String email; //dfsfsfsfd   zxczxz@dfsfsd.com   /// email : dagjgajdga
 
+    private String phoneNumber;
 
-private  Integer grade;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy HH:mm:ss", timezone = "Turkey")
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime createDate = LocalDateTime.now();
 
-
-@Column(nullable = false, length = 50, unique = true)
-@Email(message = "Provide valid email")
-private String email;
-
-
-private String phoneNummer;
-
-
-@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy HH:mm:ss", timezone = "Turkey")
-@Setter(AccessLevel.NONE)
-private LocalDateTime createDate= LocalDateTime.now();
-
+    @OneToMany(mappedBy = "student")
+    private List<Book> books = new ArrayList<>();
 
 }
